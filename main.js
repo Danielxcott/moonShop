945bf0d11f554632a51a983adf1b0edca1bcd95f
Danielxcott <changwilliam826@gmail.com>
2022-09-27 01:19:30 +0630

    added footer

--- main.js
+++ main.js
@@ -26,14 +26,13 @@ for(let child in children )
     }
 }
 products.forEach((value)=>{
-    console.log(value)
     value.addEventListener("mousemove",e=>{
-        if (e.path[2].classList.contains("col-12","col-lg-3"))
+        if (e.path[2].classList.contains("col-12"||"col-lg-3"))
         {
             e.path[2].childNodes[1].childNodes[5].classList.add("show");
             e.path[2].childNodes[3].childNodes[5].classList.add("active");
             e.path[2].childNodes[3].childNodes[3].childNodes[0].classList.add("active")
-        }else if(e.path[3].classList.contains("col-12","col-lg-3")) {
+        }else if(e.path[3].classList.contains("col-12"||"col-lg-3")) {
             e.path[3].childNodes[1].childNodes[5].classList.add("show");
             e.path[3].childNodes[3].childNodes[5].classList.add("active");
             e.path[3].childNodes[3].childNodes[3].childNodes[0].classList.add("active")
@@ -42,19 +41,19 @@ products.forEach((value)=>{
 })
 products.forEach((value)=>{
     value.addEventListener("mouseout",e=>{
-        if (e.path[2].classList.contains("col-12","col-lg-3"))
+        if (e.path[2].classList.contains("col-12"||"col-lg-3"))
         {
            setTimeout(_=>{
                e.path[2].childNodes[1].childNodes[5].classList.remove("show");
                e.path[2].childNodes[3].childNodes[5].classList.remove("active");
                e.path[2].childNodes[3].childNodes[3].childNodes[0].classList.remove("active")
-           },1000)
-        }else if(e.path[3].classList.contains("col-12","col-lg-3")) {
+           },1500)
+        }else if(e.path[3].classList.contains("col-12"||"col-lg-3")) {
             setTimeout(_=>{
                 e.path[3].childNodes[1].childNodes[5].classList.remove("show");
                 e.path[3].childNodes[3].childNodes[5].classList.remove("active");
                 e.path[3].childNodes[3].childNodes[3].childNodes[0].classList.remove("active")
-            },1000)
+            },1500)
         }
     })
 })

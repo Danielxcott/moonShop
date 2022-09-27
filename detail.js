1306eb4340e6930ebbb73d1081c127dbd40afbc1
Danielxcott <changwilliam826@gmail.com>
2022-09-27 13:06:10 +0630

    added detail page with breadcrumb

---
+++ detail.js
@@ -0,0 +1,16 @@
+import './style.scss';
+
+import * as bootstrap from 'bootstrap';
+
+
+let searchBox = document.querySelector(".search-box");
+
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("bi-search") || e.target.classList.contains("searchInput")){
+        searchBox.classList.remove("d-none");
+    }
+    else
+    {
+        searchBox.classList.add("d-none");
+    }
+})
--- main.js
+++ main.js
@@ -26,18 +26,17 @@ for(let child in children )
     }
 }
 products.forEach((value)=>{
-    value.addEventListener("mousemove",e=>{
-        if (e.path[2].classList.contains("col-12"||"col-lg-3"))
-        {
-            e.path[2].childNodes[1].childNodes[5].classList.add("show");
-            e.path[2].childNodes[3].childNodes[5].classList.add("active");
-            e.path[2].childNodes[3].childNodes[3].childNodes[0].classList.add("active")
-        }else if(e.path[3].classList.contains("col-12"||"col-lg-3")) {
-            e.path[3].childNodes[1].childNodes[5].classList.add("show");
-            e.path[3].childNodes[3].childNodes[5].classList.add("active");
-            e.path[3].childNodes[3].childNodes[3].childNodes[0].classList.add("active")
-        }
-    })
+        value.addEventListener("click", e => {
+            if (e.path[2].classList.contains("col-12" || "col-lg-3")) {
+                e.path[2].childNodes[1].childNodes[5].classList.add("show");
+                e.path[2].childNodes[3].childNodes[5].classList.add("active");
+                e.path[2].childNodes[3].childNodes[3].childNodes[0].classList.add("active")
+            } else if (e.path[3].classList.contains("col-12" || "col-lg-3")) {
+                e.path[3].childNodes[1].childNodes[5].classList.add("show");
+                e.path[3].childNodes[3].childNodes[5].classList.add("active");
+                e.path[3].childNodes[3].childNodes[3].childNodes[0].classList.add("active")
+            }
+        })
 })
 products.forEach((value)=>{
     value.addEventListener("mouseout",e=>{
@@ -47,13 +46,13 @@ products.forEach((value)=>{
                e.path[2].childNodes[1].childNodes[5].classList.remove("show");
                e.path[2].childNodes[3].childNodes[5].classList.remove("active");
                e.path[2].childNodes[3].childNodes[3].childNodes[0].classList.remove("active")
-           },1500)
+           },1200)
         }else if(e.path[3].classList.contains("col-12"||"col-lg-3")) {
             setTimeout(_=>{
                 e.path[3].childNodes[1].childNodes[5].classList.remove("show");
                 e.path[3].childNodes[3].childNodes[5].classList.remove("active");
                 e.path[3].childNodes[3].childNodes[3].childNodes[0].classList.remove("active")
-            },1500)
+            },1200)
         }
     })
 })

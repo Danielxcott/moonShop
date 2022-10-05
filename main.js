0a6f11e2a064d3cb3d03709d076657150eb9f349
Danielxcott <changwilliam826@gmail.com>
2022-10-06 02:00:26 +0630

    added price filter js

--- main.js
+++ main.js
@@ -2,6 +2,99 @@ import './style.scss';
 
 import * as bootstrap from 'bootstrap';
 
+let productLists = document.querySelector(".products")
+let product = document.getElementsByClassName("products")[0].children;
+let lists = Array.from(product)
+let children = [...productLists.childNodes];
+let products = [];
+const range = document.querySelectorAll(".range-slider input");
+const progress = document.querySelector(".range-slider .progress-slider");
+let gap = 10;
+let number = document.querySelectorAll(".progress-value");
+
+    range.forEach(input=>{
+        input.addEventListener("input",e=>{
+            let minRange = parseInt(range[0].value);
+            let maxRange = parseInt(range[1].value);
+            showProduct(minRange,maxRange);
+           if(maxRange - minRange < gap)
+           {
+            if(e.target.className === "range-min")
+            {
+                range[0].value = maxRange - gap;
+            }else
+            {
+                range[1].value = minRange + gap;
+            }
+           }else
+           {
+            progress.style.left = (minRange / range[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxRange / range[1].max) * 100 + "%";
+            number[0].innerText = minRange;
+            number[1].innerText = maxRange;
+           }
+        })
+    })
+
+    let maxPrice = 0;
+    let minPrice = 0;
+    function findMax()
+    {
+        lists.forEach(item=>{
+            if(Number(item.dataset.price )> maxPrice)
+            {
+                maxPrice = Number(item.dataset.price);
+            }
+        })
+        console.log(maxPrice)
+        return maxPrice;
+    }
+    function findMin()
+    {
+        lists.filter(item=>{
+            if(Number(item.dataset.price ) != 0)
+            {
+                if(Number(item.dataset.price ) < maxPrice)
+            {
+                minPrice = Number(item.dataset.price);
+            }
+            }
+        })
+        return minPrice;
+    }
+       findMax();
+       findMin();
+
+function showProduct(min,max)
+{
+    let noti = document.querySelector(".noti-product");
+    let lists = Array.from(product)
+    lists.filter(item=>{
+        if(item.dataset.price >= min && item.dataset.price <= max && item.dataset.price != 0)
+        {
+            // if(item.dataset.price >= min || item.dataset.price <= max){
+            //     item.classList.remove("hide");
+            //     item.classList.add("show");
+            // }else
+            // {
+            //     item.classList.add("hide");
+            // }
+            item.classList.remove("hide");
+            item.classList.add("show");
+        }else if(item.dataset.price > min || item.dataset.price >= max || min>maxPrice || max<minPrice)
+        {
+            item.classList.remove("show");
+            item.classList.add("hide");
+            noti.classList.remove("hide")
+            noti.classList.add("show")
+        }else
+        {
+            item.classList.remove("show");
+            item.classList.add("hide");
+        }
+    })
+}
+
 
 let searchBox = document.querySelector(".search-box");
 
@@ -15,9 +108,7 @@ document.addEventListener("click",(e)=>{
     }
 })
 
-let productLists = document.querySelector(".products")
-let children = [...productLists.childNodes];
-let products = [];
+
 for(let child in children )
 {
     if(child % 2 !== 0)

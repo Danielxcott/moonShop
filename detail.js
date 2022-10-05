662889570e6d5494b2cd0363f26d6947837947ac
Danielxcott <changwilliam826@gmail.com>
2022-10-05 14:40:12 +0630

    added slider js

--- detail.js
+++ detail.js
@@ -1,6 +1,6 @@
 import './style.scss';
 import * as bootstrap from 'bootstrap';
-import $ from 'jquery'
+import $, { each } from 'jquery'
 window.jQuery = window.$ = $;
 
 let searchBox = document.querySelector(".search-box");
@@ -15,3 +15,52 @@ document.addEventListener("click",(e)=>{
     }
 })
 
+let thumbnails = document.getElementsByClassName("thumbnail");
+let activeImg = document.getElementsByClassName("thumbnail active");
+let feature = document.getElementsByClassName("feature");
+
+let eachThumbnail = Array.from(thumbnails);
+for(let x of eachThumbnail)
+{
+    if(x.classList.contains("active"))
+    {
+        feature[0].src = x.src;
+    }
+}
+for(let i = 0; i<thumbnails.length ; i++)
+{
+    thumbnails[i].addEventListener("mouseover",function(){
+        if(activeImg.length > 0)
+        {
+            activeImg[0].classList.remove("active");
+        }
+            this.classList.add("active");
+            document.getElementsByClassName("feature")[0].src = this.src
+        if(activeImg.length > 0)
+        {
+            feature[0].src = this.src;
+        }        
+    })
+}
+
+let upBtn = document.querySelector(".upBtn");
+let downBtn = document.querySelector(".downBtn");
+
+downBtn.addEventListener('click',function(e){    
+    document.querySelector(".slider").scrollTop += 180;
+})
+upBtn.addEventListener('click',function(e){    
+    document.querySelector(".slider").scrollTop -= 180;
+})
+
+console.log(window.innerWidth);
+
+let leftBtn = document.querySelector(".leftBtn");
+let rightBtn = document.querySelector(".rightBtn");
+
+leftBtn.addEventListener('click',function(e){    
+    document.querySelector(".slider").scrollLeft -= 180;
+})
+rightBtn.addEventListener('click',function(e){    
+    document.querySelector(".slider").scrollLeft += 180;
+})
